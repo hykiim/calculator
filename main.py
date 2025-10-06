60f0e38826a3c2de55be1e1ee5a189897cc5f1cc
anilo97 <anilo97@naver.com>
2025-10-06 14:32:19 +0900

    Add text edit te1 and fix btn1 handler function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox) #QMessageBox : 메시지박스 위젯
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) #QMessageBox : 메시지박스 위젯
 from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리 
 
 class Calculator(QWidget):
@@ -8,11 +8,14 @@ class Calculator(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
+        self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 하도록 수정 
+
         self.btn1 = QPushButton('Message', self) #버튼 추가
         self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭 시 핸들러 함수 연결
 
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
-        vbox.addStretch(1)# 빈 공간
+        vbox.addWidget(self.te1)# 빈 공간
         vbox.addWidget(self.btn1) # 버튼 위치
         vbox.addStretch(1) # 빈 공간
 
@@ -24,7 +27,8 @@ class Calculator(QWidget):
         self.show()
 
     def activateMessage(self): #버튼을 클릭할 때 동작하는 함수: 메시지 박스 출력 
-        QMessageBox.information(self, "information", "Button clicked!")
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
