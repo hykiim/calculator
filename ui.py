e8e65ebf6f6491ec544feffaf0222ff07a21cd45
anilo97 <anilo97@naver.com>
2025-10-12 03:03:39 +0900

    Modify ui.py to add le1, le2 and vb widgets

--- ui.py
+++ ui.py
@@ -1,15 +1,20 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel) #QLabel 추가
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox) # QLineEdit, QComboBox 추가
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDate, Qt # 날짜와 주요 속성값 사용을 위해 추가 
+from PyQt5 import QtCore # 모듈 추가
 
 class View(QWidget):
     def __init__(self):
         super().__init__()
-        self.date = QDate.currentDate() # 현재 날짜를 저장하기 위해 추가
         self.initUI()
 
     def initUI(self):
-        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate), self) #추가
+        self.le1=QLineEdit('0', self) #라인 에디트1 추가
+        self.le1.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트1 문자열 배치 설정 
+        self.le2=QLineEdit('0', self) #라인 에디트2 추가
+        self.le2.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트2 문자열 배치 설정 
+
+        self.cb = QComboBox(self) #콤포 박스 추가
+        self.cb.addItems(['+','-','*','/']) # 콤보 박스 항목 추가(연산자로 사용)
 
         self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
         self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 하도록 수정 
@@ -21,11 +26,15 @@ class View(QWidget):
         hbox.addStretch(1) #공백
         hbox.addWidget(self.btn1) #버튼1 배치
         hbox.addWidget(self.btn2) #버튼2 배치
+        hbox_formular = QHBoxLayout() # 새로 정의한 위젯을 QHBoxLayout에 배치
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
 
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.te1)# 빈 공간
+        vbox.addLayout(hbox_formular) # hbox_formular 배치
         vbox.addLayout(hbox)
-        vbox.addWidget(self.lbl1) #수정
         vbox.addStretch(1) # 빈 공간        
         
         self.setLayout(vbox) # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
