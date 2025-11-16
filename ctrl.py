fe431ef33b1a4340f0dc69fa98511871eefa3860
anilo97 <anilo97@naver.com>
2025-11-16 22:29:48 +0900

    Add sub, mul, div, pow function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -17,7 +17,17 @@ class Control:
         self.view.btn1.clicked.connect(lambda: self.view.setDisplay(self.calculate()))
         self.view.btn2.clicked.connect(self.view.clearMessage)
 
-    def sum(
-        self, a, b
-    ):  # 예외 처리 제거: 향후 calculate  함수에서 처리하도록 구현 예정
+    def sum(self, a, b):
         return a + b
+
+    def sub(self, a, b):  # 뺄샘 함수 추가
+        return a - b
+
+    def mul(self, a, b):  # 곱셈 함수 추가
+        return a * b
+
+    def div(self, a, b):  # 나눗셈 함수 추가
+        return a / b
+
+    def pow(self, a, b):  # 제곱 연산 함수 추가
+        return pow(a, b)
